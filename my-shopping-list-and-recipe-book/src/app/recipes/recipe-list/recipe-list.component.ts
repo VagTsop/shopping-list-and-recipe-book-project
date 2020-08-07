9a26b464bb4843f879882ec361faee23430de0d4
vagg tsop <vatsop52@gmail.com>
2020-08-07 03:09:39 +0300

    finished services

--- my-shopping-list-and-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-shopping-list-and-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- @Output() recipeWasSelected =  new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-     new Recipe('A Test Recipe', 'This is simply a test',
-     'https://i2.wp.com/wholefully.com/wp-content/uploads/2019/11/homemade-hot-sauce-bottled-800x1200.jpg')
-    
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+
 
 }
