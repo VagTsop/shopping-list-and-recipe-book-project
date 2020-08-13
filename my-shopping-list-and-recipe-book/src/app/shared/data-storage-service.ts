8206ca55ebdbd4eb7572180634f8ce76cb86b1f5
vagg tsop <vatsop52@gmail.com>
2020-08-14 01:53:24 +0300

    resolving data before loading

--- my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
+++ my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -23,17 +23,19 @@ export class  DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+      return  this.http
         .get<Recipe[]>('https://shopping-list-recipe-boo-f8884.firebaseio.com/recipes.json')
         .pipe(map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
             });
-        }))
-        .subscribe(recipes => {
-           console.log(recipes);
-           this.recipeService.setRecipes(recipes);
-        })
+        }),
+        tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+
+         })
+        )
+      
     }
 
 }
