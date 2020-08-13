1f64a40d2eb508be851699e13de18ed940820d53
vagg tsop <vatsop52@gmail.com>
2020-08-14 01:08:20 +0300

    fetch recipes from firebase

--- my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
+++ my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class  DataStorageService {
@@ -10,7 +10,9 @@ export class  DataStorageService {
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
-        return  this.http.put('https://shopping-list-recipe-boo-f8884.firebaseio.com/recipes.json',
+        this.http
+          .put(
+              'https://shopping-list-recipe-boo-f8884.firebaseio.com/recipes.json',
          recipes 
      )
         
@@ -18,4 +20,14 @@ export class  DataStorageService {
         console.log(response);
      });
     }
+
+    fetchRecipes() {
+        this.http
+        .get<Recipe[]>('https://shopping-list-recipe-boo-f8884.firebaseio.com/recipes.json')
+        .subscribe(recipes => {
+           console.log(recipes);
+           this.recipeService.setRecipes(recipes);
+        })
+    }
+
 }
