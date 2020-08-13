462e37259887b613fcc7858bebbf63b459cffdee
vagg tsop <vatsop52@gmail.com>
2020-08-13 21:29:20 +0300

    save recipes to firebase

--- my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
+++ my-shopping-list-and-recipe-book/src/app/shared/data-storage-service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable()
 export class  DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
     
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        return  this.http.put('https://shopping-list-recipe-boo-f8884.firebaseio.com/recipes.json',
+         recipes 
+     )
+        
+     .subscribe(response =>{
+        console.log(response);
+     });
+    }
 }
