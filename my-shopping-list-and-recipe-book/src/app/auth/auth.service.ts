02e27543de1b9fd6cf8821f3a23f8a78db819cd2
vagg tsop <vatsop52@gmail.com>
2020-08-15 05:58:52 +0300

    adding auth guard

--- my-shopping-list-and-recipe-book/src/app/auth/auth.service.ts
+++ my-shopping-list-and-recipe-book/src/app/auth/auth.service.ts
@@ -44,7 +44,7 @@ export class AuthService {
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('add your firebase endpoint for login in with email/password',
+        return this.http.post<AuthResponseData>('add your firebase endpoint for login with your email/password',
             {
                 email: email,
                 password: password,
